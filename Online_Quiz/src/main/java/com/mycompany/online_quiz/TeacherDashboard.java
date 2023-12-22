3c71dda91855582fa0ab22aa14c7b355df621241
zaermichael.vanzuela <152011319+zaermichaelvanzuela@users.noreply.github.com>
2023-12-22 23:01:15 +0800

    Update TeacherDashboard.java

--- Online_Quiz/src/main/java/com/mycompany/online_quiz/TeacherDashboard.java
+++ Online_Quiz/src/main/java/com/mycompany/online_quiz/TeacherDashboard.java
@@ -22,7 +22,7 @@ public class TeacherDashboard extends JFrame {
     private JButton logOutButton; // Added logout button
      private JButton createButton;
 
-   public TeacherDashboard(Connection connection, String username) {
+   public TeacherDashboard(Connection connection, int username) {
     this.connection = connection;  // Make sure this line is present
     this.loggedInUsername = username;
     initComponents();
